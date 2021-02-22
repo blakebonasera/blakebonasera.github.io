d2ab21fab6370e9af2f1d5c2a16bfb4d489bb9ba
Blake <blakebonasera@gmail.com>
2021-02-22 11:30:44 -0800

    decided to relearn reactstrap for portfolio

---
+++ src/views/Jumbotron.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Navigation from '../components/NavBar'
+import { Jumbotron, Container } from "reactstrap";
+
+const Jumbo = (props) => {
+    return (
+        <div>
+            <Jumbotron fluid>
+                <Container fluid>
+                    <h1 className="display-3">Blake Bonasera</h1>
+                    <p className="lead">"You miss 100% of the shots you don't take. - Wayne Gretzky" - Michael Scott</p>
+                    <Navigation />
+                </Container>
+            </Jumbotron>
+        </div>
+    );
+};
+
+export default Jumbo;
