35429760d7e56f6e498e3a86370939c708212bbf
Blake <blakebonasera@gmail.com>
2021-03-02 16:25:39 -0800

    misspell

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Jumbo from "./views/Jumbotron";
 import AboutMe from "./views/AboutMe";
 import Resume from "./views/Resume";
-import ForSale from './views/4Sale';
+import ForSale from './views/4sale';
 import Apartment from './components/AptConCarousel';
 import Footer from './views/Footer';
 import { Router } from "@reach/router";
